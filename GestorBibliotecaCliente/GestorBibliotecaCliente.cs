6839c5f55f15bad3c11504d38819efcd0c4bd255
AlePerez132 <alepd132@gmail.com>
2024-05-23 12:32:04 +0200

    primer commit

---
+++ GestorBibliotecaCliente/GestorBibliotecaCliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace GestorBibliotecaComun
+{
+    class GestorBibliotecaCliente
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
